46f410f0f498b65afd8ac232d74d04baa85633d4
Lauren McCarthy <laurmccarthy@gmail.com>
2017-09-19 12:45:03 -0700

    pull

--- git-pull.php
+++ git-pull.php
@@ -1,5 +1,12 @@
 <?php
-echo system('git reset --hard');
-echo system('git checkout -- .');
-echo system('git pull https://github.com/lmccart/DMA403-gradcrit.git master');
+// echo system('git reset --hard');
+// echo system('git checkout -- .');
+// echo system('git pull https://github.com/lmccart/DMA403-gradcrit.git master');
+
+require_once('Git.php');
+
+echo 'pulling latest';
+$repo = Git::open('https://github.com/lmccart/DMA403-gradcrit.git'); 
+$repo->pull('origin', 'master');
+
 ?>
